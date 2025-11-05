b32567a8c44f1903bae361f5257e21ee1cdfe764
Derek Stringer <derekstringer@Dereks-MacBook-Air.local>
2025-11-04 19:59:31 -0600

    Revert 'fix: maintain scroll position on backward pagination'
    This reverts commit 01396bc.
    
    User reports backward scroll is now blocked at arbitrary date (Sept 20th).
    Reverting maintainVisibleContentPosition prop to restore previous working behavior.

--- src/screens/home/DailyV3.tsx
+++ src/screens/home/DailyV3.tsx
@@ -822,19 +822,15 @@ export const DailyV3: React.FC<DailyV3Props> = ({
           initialScrollIndex={initialScrollIndex}
           onViewableItemsChanged={onViewableItemsChanged}
           viewabilityConfig={viewabilityConfig}
-          removeClippedSubviews={false}
-          windowSize={10}
-          initialNumToRender={20}
-          maxToRenderPerBatch={10}
-          updateCellsBatchingPeriod={50}
-          onScroll={handleScroll}
-          scrollEventThrottle={400}
-          onEndReached={loadMoreForward}
-          onEndReachedThreshold={0.5}
-          maintainVisibleContentPosition={{
-            minIndexForVisible: 0,
-            autoscrollToTopThreshold: 10,
-          }}
+        removeClippedSubviews={false}
+        windowSize={10}
+        initialNumToRender={20}
+        maxToRenderPerBatch={10}
+        updateCellsBatchingPeriod={50}
+        onScroll={handleScroll}
+        scrollEventThrottle={400}
+        onEndReached={loadMoreForward}
+        onEndReachedThreshold={0.5}
         keyboardDismissMode="on-drag"
         keyboardShouldPersistTaps="handled"
         getItemLayout={(data, index) => {
